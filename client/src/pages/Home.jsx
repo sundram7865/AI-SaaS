b5291c11f44a3dbec8551bd394873c4c44d328d6
sundram <ram322222@gmail.com>
2025-07-13 12:58:36 +0530

    added billing plan

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,6 +3,7 @@ import Navbar from '../components/Navbar'
 import Hero from '../components/Hero'
 import AiTools from '../components/AiTools'
 import Testimonial from '../components/Testimonial'
+import Plan from '../components/Plan'
 
 const Home = () => {
   return (
@@ -11,7 +12,9 @@ const Home = () => {
     <Hero/>
     <AiTools/>
     <Testimonial/>
+    <Plan/>
     </>
+
   )
 }
 
