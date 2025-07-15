e842da0da6460e3d37685e1b4abb1a84118ff40d
sundram <ram322222@gmail.com>
2025-07-15 09:55:15 +0530

    updated server.js after user routes

--- server/server.js
+++ server/server.js
@@ -1,10 +1,14 @@
 import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
-import { clerkMiddleware ,requireAuth} from '@clerk/express'
+import { clerkMiddleware, requireAuth } from '@clerk/express'
+import aiRouter from './routes/aiRoutes.js';
+import connectCloudinary from './configs/cloudinary.js';
+import userRouter from './routes/userRoutes.js';
 
 const app = express()
 
+await connectCloudinary()
 
 app.use(cors())
 app.use(express.json())
@@ -13,6 +17,9 @@ app.use(clerkMiddleware())
 app.get('/', (req, res)=>res.send('Server is Live!'))
 
 app.use(requireAuth())
+
+app.use('/api/ai', aiRouter)
+app.use('/api/user', userRouter)
 
 const PORT = process.env.PORT || 3000;
 
