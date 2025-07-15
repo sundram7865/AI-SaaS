7d98db2b705ddd8b9c28b49100570fffff951bec
sundram <ram322222@gmail.com>
2025-07-15 09:18:36 +0530

    added clerk middleware to server

--- server/server.js
+++ server/server.js
@@ -1,18 +1,18 @@
 import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
-
+import { clerkMiddleware ,requireAuth} from '@clerk/express'
 
 const app = express()
 
 
 app.use(cors())
 app.use(express.json())
-
+app.use(clerkMiddleware())
 
 app.get('/', (req, res)=>res.send('Server is Live!'))
 
-
+app.use(requireAuth())
 
 const PORT = process.env.PORT || 3000;
 
